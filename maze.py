67cfa3cf835f3f58a7212936a51a7a97473f6b0a
MQCyrusKwan <cyrus.kwan@outlook.com>
2024-08-27 16:46:47 +1000

    clear canvas on draw_grid to save memory

--- maze.py
+++ maze.py
@@ -1,23 +1,21 @@
 from cell import Cell
 import tkinter as tk
-import time
 
-class Maze():
+class Maze:
     def __init__(self, rows:int=None, cols:int=None, file_name:str=None):
         self.rows:int = rows
         self.cols:int = cols
 
         if file_name:
-            print("importing")
-            self.grid = self.import_grid(file=file_name)
+            self.grid:list[list] = self.import_grid(file=file_name)
         else:
-            print("generating")
-            self.grid = self.create_grid()
+            self.grid:list[list] = self.create_grid()
 
         self.width:int = self.cols * Cell.size
         self.height:int = self.rows * Cell.size
     
     def draw_grid(self, canvas:tk.Canvas):
+        canvas.delete("all")
         for row in range(self.rows):
             for col in range(self.cols):
                 x1:int = col * Cell.size
@@ -33,7 +31,7 @@ class Maze():
             columns:str = ","+",".join([f"{i}" for i in range(len(self.grid))])
             matrix:list[str] = [columns]
             for r, row in enumerate(self.grid):
-                items:list[str] = [f"{int(cell.wall)}" for cell in row]
+                items:list[str] = [f"{int(cell.path)}" for cell in row]
                 line:str = f"{r},"+",".join(items)
                 matrix.append(line)
             lines:str = "\n".join(matrix)
@@ -43,14 +41,14 @@ class Maze():
         with open(file=file, mode="r") as file:
             string:str = file.read()
             matrix:list = [row.split(",")[1:] for row in string.split("\n")[1:]]
-            self.rows = len(matrix)
-            self.cols = len(matrix[0])
+            self.rows:int = len(matrix)
+            self.cols:int = len(matrix[0])
 
-        grid = [[] for line in matrix]
+        grid:list[list] = [[] for line in matrix]
         for r, row in enumerate(matrix):
             for c, col in enumerate(matrix[r]):
-                wall = True if int(matrix[r][c]) == 1 else False
-                grid[r].append(Cell(row=r, col=c, wall=wall))
+                path = True if int(matrix[r][c]) == 1 else False
+                grid[r].append(Cell(row=r, col=c, path=path))
 
         return grid
     
@@ -60,10 +58,11 @@ class Maze():
 
 def main():
     root = tk.Tk()
-    maze = Maze(file_name="test")
+    maze = Maze(rows=20, cols=20)
     canvas = tk.Canvas(root, width=maze.width, height=maze.height)
     canvas.pack()
 
+    maze.export_grid("maze.csv")
     maze.draw_grid(canvas=canvas)
     # while True:
     #     maze.new_grid()
